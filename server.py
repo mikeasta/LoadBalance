eb94b38d9977352cba4b7b7f07d5e8cce0f4c9d3
mikeasta <thenaitrise@gmail.com>
2022-10-06 21:47:32 +0300

    Little better, but the same problem.

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import asyncio
 import logging
 from aiohttp import web, ClientSession
 import requests
+import time
 
 
 # Params
@@ -17,21 +18,14 @@ app    = web.Application()
 routes = web.RouteTableDef()
 
 
-# root func
-@routes.get('/')
-def hello_world(self):
-    return web.Response(text="/")
-
-
+# * Utils
 # Get request path
-def getRequestPath(port):
+def getRequestUrl(port):
     return f"http://{address}:{port}"
-
 
 # Check if there is any available server
 def isAvailablePort():
     return not len(server_queue) == len(ports)
-
 
 # Look for available port
 def getAvailablePort():
@@ -40,50 +34,61 @@ def getAvailablePort():
             return port
 
 
-# * Pinger
+# * API
+# Root func
+def hello_world(self):
+    return web.Response(text="/")
+
+
 # Getting data from special worker
-async def fetch(path, session):
-    async with session.get(path) as resp:
+async def fetch(url, session):
+    async with session.get(url) as resp:
         return await resp.text()
+    
 
 
 # Listener
-@routes.get('/pinger')
 async def pinger(self):
-    response = 'null'
-
+    response = ''
+    
     # Check if all servers are working
-    if not isAvailablePort():
-        request_queue.append(1)
-        logging.info("All servers are busy. Your request will be performed later.")
+    if isAvailablePort():
+        # Look for available ports
+        port = getAvailablePort()
+
+        # Getting prepared server url   
+        url = getRequestUrl(port)
+
+        # Adding server to busy-servers-list
+        # Performs request to that server
+        server_queue.append(port)
+
+        # Get result from workers
+        async with ClientSession() as session:
+            response = await fetch(url, session)
+        server_queue.remove(port)
+
+        # Logging response
+        logging.info(response)
+        if bool(request_queue):
+            context = request_queue.pop()
+            await pinger(context)
+
+        # Result
+        logging.info(time.ctime(time.time()))
+        return web.Response(text=response, status=200)
+    else: 
+        request_queue.append(self)
+        logging.info("All Servers Are Busy Now!")
         return web.Response(text=response, status=200)
 
-    # Look for available ports
-    port = getAvailablePort()
 
-    # Getting prepared server url   
-    path = getRequestPath(port)
+# * Server start-up
+app.add_routes([
+    web.get('/', hello_world),
+    web.get('/pinger', pinger)
+])
 
-    # Adding server to busy-servers-list
-    # Performs request to that server
-    server_queue.append(port)
-    async with ClientSession() as session:
-        response = await fetch(path, session)
-    server_queue.remove(port)
-
-    # Logging response
-    logging.info(response)
-
-    # Check if there are necessary to make new request
-    if bool(request_queue):
-        request_queue.pop()
-        return await pinger(self)
-    
-    # Result
-    return web.Response(text=response, status=200)
-
-
-app.add_routes(routes)
-if __name__ == "__main__":
+if __name__ == "__main__":    
     logging.basicConfig(level=logging.INFO)
     web.run_app(app, host=address, port=default_port)
