63cf093c05ea13879974bddf0393398e155de284
mikeasta <thenaitrise@gmail.com>
2022-10-11 13:03:43 +0300

    some kind of done

--- server.py
+++ server.py
@@ -1,4 +1,7 @@
+from ast import Num, Str
 import asyncio
+from typing import Literal
+from xmlrpc.client import Boolean
 from aiojobs.aiohttp import setup, spawn
 import logging
 from aiohttp import web, ClientSession
@@ -21,15 +24,15 @@ routes = web.RouteTableDef()
 
 # * Utils
 # Get request path
-def getRequestUrl(port):
+def getRequestUrl(port: int) -> str:
     return f"http://{address}:{port}"
 
 # Check if there is any available server
-def isAvailablePort():
+def isAvailablePort() -> bool:
     return not len(server_queue) == len(ports)
 
 # Look for available port
-def getAvailablePort():
+def getAvailablePort() -> int:
     for port in ports:
         if port not in server_queue:
             return port
@@ -42,7 +45,7 @@ def hello_world(self):
 
 
 # Getting data from special worker
-async def fetch():
+async def fetch(session):
     response = ''
 
     # Look for available ports
@@ -55,9 +58,8 @@ async def fetch():
     # Performs request to that server
     server_queue.append(port)
 
-    async with ClientSession() as session:
-        async with session.get(url) as resp:
-            response = await resp.text()
+    async with session.get(url) as resp:
+        response = await resp.text()
     
     server_queue.remove(port)
     return response
@@ -67,25 +69,21 @@ async def fetch():
 async def pinger(req):
     response = ''
     
+    while not isAvailablePort():
+        await asyncio.sleep(1)
+
     # Check if all servers are working
     if isAvailablePort():
-        
+
         # Getting data
-        response = await fetch()
+        async with ClientSession() as session:
+            response = await fetch(session)
 
         # Logging response
         logging.info(response)
-
-        if bool(request_queue):
-            context = request_queue.pop(0)
-            await spawn(req, pinger(context))
         
         # Result
         logging.info(time.ctime(time.time()))
-        return web.Response(text=response, status=200)
-    else: 
-        request_queue.append(req)
-        logging.info("All Servers Are Busy Now!")
         return web.Response(text=response, status=200)
 
 
